83bb31a211962d793f111ea254516de08f1ef232
Akhilesh1902 <akhil1922002@gmail.com>
2022-02-07 18:32:00 +0530

    gsap animation added to landing page

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import "./css/style.css";
 import gsap from "gsap";
 console.log(gsap);
 
-let tl = gsap.timeline();
+let tl = gsap.timeline({ defaults: { ease: "power4.easeInOut", duration: 2 } });
 console.log(tl);
+
+tl.to(".bg", { scale: 1, opacity: 1, duration: 2.2 });
+tl.to(".hero-content h1", { y: 0, opacity: 1 }, "-=2");
+tl.to(".hero-content p", { y: 0, opacity: 1 }, "-=1.4");
