522fbda86a22fef47bf0a4d33488dca0cbf39e79
Akhilesh1902 <akhil1922002@gmail.com>
2022-02-07 19:32:56 +0530

    Added netlify form submission

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ console.log(gsap);
 let tl = gsap.timeline({ defaults: { ease: "power4.easeInOut", duration: 2 } });
 console.log(tl);
 
-tl.to(".bg", { scale: 1, opacity: 1, duration: 2.2 });
-tl.to(".hero-content h1", { y: 0, opacity: 1 }, "-=2");
-tl.to(".hero-content p", { y: 0, opacity: 1 }, "-=1.4");
+tl.to(".bg", { scale: 1, opacity: 1, duration: 1.8 });
+tl.to(".hero-content h1", { y: 0, opacity: 1 }, "-=1.6");
+tl.to(".hero-content p", { y: 0, opacity: 1 }, "-=1.3");
